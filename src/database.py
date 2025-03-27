dd5c6587d57189fb40f6a35a8ded07d211b91495
Sobika <subuadhikari03@gmail.com>
2025-03-27 23:00:40 +0545

    add_transcation,get_transaction sql queries added

--- src/database.py
+++ src/database.py
@@ -1,5 +1,6 @@
 import sqlite3
 def init_db():
+
     conn=sqlite3.connect("transaction.db")
     cursor=conn.cursor()
     cursor.execute(''' CREATE TABLE IF NOT EXISTS transactions(
@@ -14,4 +15,27 @@ def init_db():
     ''')
     conn.commit()
     conn.close()
+def add_transaction(date,t_type,amount,category,description):
+   
+
+    conn=sqlite3.connect("transaction.db")
+    cursor=conn.cursor()
+    cursor.execute('''INSERT INTO transactions (date,type,amount,category,description)
+    VALUES(?,?,?,?,?)
+    ''',(date,t_type,amount,category,description))
+    
+    conn.commit()
+    conn.close()
+def get_transactions():
+    conn=sqlite3.connect("transaction.db")
+    cursor=conn.cursor()
+    cursor.execute('''SELECT * FROM transactions
+    ''')    
+    transactions=cursor.fetchall()
+    conn.close()
+    return transactions
+        
 init_db()
+add_transaction("2025-03-27","income","15000","Grocery"," ")
+all_transaction=get_transactions()
+print(all_transaction)
